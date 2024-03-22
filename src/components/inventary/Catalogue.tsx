9679782f40a08c9e9237e36e6ba0e098c31f26d5
Camilo Yepes <camyep20@gmail.com>
2024-03-21 23:12:04 -0500

    Start Catalogue

---
+++ src/components/inventary/Catalogue.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Catalogue = () => {
+  return (
+    <View>
+      <Text>
+        Inicio Catalogo
+      </Text>
+    </View>
+  )
+}
+
+export default Catalogue
