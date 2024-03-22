c96350c9f8168b88b46923bb3d3587b0ada8dd69
Camilo Yepes <camyep20@gmail.com>
2024-03-21 22:46:22 -0500

    Creation Proyect

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>
+        Hola mundo, funciono esta chimbada
+      </Text>
+    </View>
+  )
+}
+
+export default App
